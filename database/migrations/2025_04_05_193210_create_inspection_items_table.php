edb3d44ea85e32aeff9f499f14654b6ddff3aa6c
Nebyu Daniel <123291979+nebyu267@users.noreply.github.com>
2025-04-05 13:08:34 -0700

    Add files via upload

---
+++ database/migrations/2025_04_05_193210_create_inspection_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+    public function up(): void
+    {
+        Schema::create('inspection_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('template_id')->constrained('inspection_templates')->onDelete('cascade');
+            $table->string('item_name');
+            $table->boolean('is_required')->default(true);
+            $table->integer('order')->default(0);
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inspection_items');
+    }
+};
